027835ecf18888a136dd44cff08dc68f4ad31a03
Victor Hernandez <vik407@gmail.com>
2020-03-11 16:04:42 -0500

    14. Balance factor 0x0

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,48 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - get the heigh of a tree
+ * @tree: input tree
+ * Return: tree heigh
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (measure(tree) - 1);
+}
+/**
+ * measure - helper heigh to measure func
+ * @tree: tree
+ * Return: the heigh
+ */
+size_t measure(const binary_tree_t *tree)
+{
+	size_t a, b;
+
+	if (!tree)
+		return (0);
+
+	a = measure(tree->left);
+	b = measure(tree->right);
+	return (MAX(a, b) + 1);
+}
+/**
+ * binary_tree_balance - function that measures the balance factor
+ * of a binary tree
+ * @tree: tree
+ * Return: measure the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int a, b;
+
+	if (!tree)
+		return (0);
+	a = (tree->left)
+		? (int)binary_tree_height(tree->left)
+		: -1;
+	b = (tree->right)
+		? (int)binary_tree_height(tree->right)
+		: -1;
+	return (a - b);
+}
