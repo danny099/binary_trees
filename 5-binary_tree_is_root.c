a6b0e25adfa471e4586ba6c8acb1edabbcfbb40d
danny099 <dalmariv01@gmail.com>
2020-03-11 05:48:48 +0000

    detect if a node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - detect if a node is a root
+ * binary_tree_is_root - detect if a node is a root
  * @node: pointer to a node
  * Return: 0 if is false or 1 if is true
  */
