cc878f2f709e07dc714e291f1973011122a2f5a3
danny099 <dalmariv01@gmail.com>
2020-03-11 09:13:40 -0500

    heigt of a node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,16 +14,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/* compute the depth of each subtree */
 	if (tree->left)
 	{
-		countLeft = binary_tree_height(tree->left);
+		countLeft = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right)
 	{
-		countRight = binary_tree_height(tree->right);
+		countRight = 1 + binary_tree_height(tree->right);
 	}
 
 	/* use the larger one */
 	if (countLeft > countRight)
-		return (countLeft + 1);
+		return (countLeft);
 	else
-		return (countRight + 1);
+		return (countRight);
 }
