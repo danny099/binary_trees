499238927338a46f82bda10c4df6abf4106b9244
danny099 <dalmariv01@gmail.com>
2020-03-11 14:03:37 -0500

    fix when node is null

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,6 +8,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	if (node == NULL)
+	{
+		return (0);
+	}
 	if (node->left != NULL && node->right != NULL && node->parent == NULL)
 		return (1);
 	else
