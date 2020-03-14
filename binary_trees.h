2b5ac3894d7c30b4d36304d4ae59c24175a1741d
Victor Hernandez <vik407@gmail.com>
2020-03-14 01:33:51 +0000

    19. Lowest common ancestor 0x1

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,8 @@
-#ifndef BYNARY_TREES
-#define BINARY_TREES
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
 #include <stdlib.h>
 #include <stddef.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,10 +13,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -43,8 +43,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 
 size_t measure(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 int max(int a, int b);
+
 #endif
